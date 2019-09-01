c639a536755c37184cb523f3d55b328dd5326840
dawnlunacy <rudd.lacy@gmail.com>
2019-09-01 12:31:20 -0600

    Add generate categories and generate players methods to game

--- src/Game.js
+++ src/Game.js
@@ -3,21 +3,37 @@ import Round from '../src/Round';
 import Player from '../src/Player';
 
 class Game {
-    constructor(data, players) {
-        this.playersList = players;
-        this.generatedPlayers;
-        this.categories;
-        this.clues;
-        this.winner;
-        this.roundCounter;
-        this.currentRound;
-    }
+  constructor(data, players) {
+    this.data = data;
+    this.playersList = players;
+    this.generatedPlayers;
+    this.allCategories = this.data.categories;
+    this.currentCategories = [];
+    this.clues;
+    this.winner;
+    this.roundCounter;
+    this.currentRound;
+  }
 
-    generatedPlayers() {
-        console.log("id:", this.playersList[playerName])
-        this.generatedPlayers = this.playersList.map(playerName => new Player(this.playersList[playerName], playerName));
+  startGame() {
+    this.generatePlayers();
+  }
 
+  generatePlayers() {
+    this.generatedPlayers = this.playersList.map(playerName => new Player(this.playersList.indexOf(playerName) + 1, playerName));
+  }
+
+  generateCategories() {
+        
+    this.allCategories = Object.keys(this.data.categories);
+    for (let i = 0; i < 4; i++) {
+      let randomCategory = Math.round(Math.random() * (this.allCategories.length));
+      this.currentCategories.push(this.allCategories[randomCategory]);
+      this.allCategories.splice(randomCategory, 1);
     }
+    return this.currentCategories;
+  }
+
 
 }
 
