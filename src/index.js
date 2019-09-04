36078676d54c4f6f81fdfc4ec62260132d1b3b47
dawnlunacy <rudd.lacy@gmail.com>
2019-09-04 17:43:11 -0600

    rename images for clearer naming for rounds and fix broken link

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import $ from 'jquery';
 import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+
+// import './images/splash.gif'
 
 console.log('This is the JavaScript entry file - your code begins here.');
