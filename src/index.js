dfa9379295919bc9fc3f57cccf7bea5647f654e7
Allison Wagner <allisonjw01@gmail.com>
2019-09-04 17:34:28 -0600

    Added imports to index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import $ from 'jquery';
 import './css/base.scss';
 
-import Game from '../Game';
-import DOMUpdates from '../DOMUpdates.js';
-import Round from '..Round'
+import Game from './Game';
+import DOMUpdate from './DOMUpdate';
+import Round from './Round'
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
@@ -13,7 +13,7 @@ fetch("https://fe-apps.herokuapp.com/api/v1/gametime/1903/jeopardy/data")
   .then(data => data)
   .catch(error => console.log(error));
 
-let game
-let round
-let player
+let game;
+let round;
+let player;
 
