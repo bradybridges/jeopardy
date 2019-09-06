df8a025fb46db5b2a421acd6862380af3317c3d9
dawnlunacy <rudd.lacy@gmail.com>
2019-09-06 15:12:02 -0600

    Create cards dynamically adding by point value

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -10,12 +10,29 @@ const domUpdates = {
   populateBoard(categories, clues) {
 
     for (let i = 0; i < 4; i++) {
-      $('.grid-item:nth-child(1)').text(categories[i].name);
-      
+      // let bob = `.grid-container .grid-item:nth-child(${i})`
+      let kim = `.grid-item:nth-child(${i + 1})`;
+      $(kim).text(categories[i].name);
     }
     
-  }
+    let sortedClues = clues.sort((a,b) => {
+      return a.pointValue - b.pointValue;
+    });
+    console.log("SortedClues", sortedClues)
 
+    for (let i = 0; i < 16; i++) {
+      // let bob = `.grid-container .grid-item:nth-child(${i})`
+      let kimbo = `.grid-item:nth-child(${i + 5})`;
+      $(kimbo).text(sortedClues[i].pointValue);
+    }
+  }
+ 
 };
 
 export default domUpdates;
+
+// for (let i = 0; i < 4; i++) {
+//     let bob = `.grid-container .grid-item:nth-child(${i})`
+//   $(bob).text(categories[i - 1].name);
+  
+// }
