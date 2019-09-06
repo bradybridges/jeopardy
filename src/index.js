3d1459a21caf6f20537c7322c71f9757ecdd5c21
dawnlunacy <rudd.lacy@gmail.com>
2019-09-05 19:07:10 -0600

    Add grid to dom and set up frame of method for appending

--- src/index.js
+++ src/index.js
@@ -9,29 +9,34 @@ import Game from './Game';
 import domUpdates from './domUpdates.js'
 
 let game;
+let data;
 
-fetch("https://fe-apps.herokuapp.com/api/v1/gametime/1903/jeopardy/data")
-  .then(response => response.json())
-  .then(data => data.data)
-  .catch(error => console.log(error));
-
+ fetch("https://fe-apps.herokuapp.com/api/v1/gametime/1903/jeopardy/data")
+    .then(dataResponse => dataResponse.json())
+    .then(dataResponse => data = dataResponse.data)
+    .catch(error => console.log(error));
 
 $(document).ready(function() {
   console.log("MOOOOOOSES")
-
+  
   $('.intake-form-inputs').keyup(function() {
     if ($('.player-one-intake').val() !== '' && $('.player-two-intake').val() !== '' && $('.player-three-intake').val() !== '') {
       $('.start-game-btn').prop('disabled', false);  
     }
   })
-
-//   $('.start-game-btn').click(function() {
-//     let player1 = $('.player-one-intake').val();
-//     let player2 = $('.player-two-intake').val();
-//     let player3 = $('.player-three-intake').val();
+  
+  $('.start-game-btn').click(function(e) {
+      e.preventDefault()
+    let player1 = $('.player-one-intake').val();
+    let player2 = $('.player-two-intake').val();
+    let player3 = $('.player-three-intake').val();
     
-//     game = new Game(data, [player1, player2, player3])
-//   })
+    game = new Game(data, [player1, player2, player3])
+    console.log("Game.data", game);
+
+    domUpdates.transitionToFirstRound();
+    domUpdates.populateBoard(game.currentCategories, game.clues)
+  })
   
 })
 
